357f5b4071de81279b768c313744003cf238f957
aslihanozfidan <aslihanozfidan@gmail.com>
2018-12-14 00:38:03 +0300

    add range style and props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import "./style.css";
 
 class RangeBox extends Component {
   render() {
+    const { step, max, min, textThumb } = this.props
     return (
       <div>
-        <input type="range" id="range-box" name="range-box" min="0" max="11" />
+        <input 
+          type="range"
+          id="range-box"
+          name="range-box"
+          min={min || 0}
+          max={max || 100}
+          step={step || 1} />
         <label for="range-box">Label</label>
       </div>
-    );
+    )
   }
 }
 
-export default RangeBox;
+export default RangeBox
