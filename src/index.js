4f60f7e83a9174b90285f2fff78d7dbb06ee9d41
aslihanozfidan <aslihanozfidan@gmail.com>
2018-12-16 16:11:14 +0300

    release version

--- src/index.js
+++ src/index.js
@@ -1,25 +1,58 @@
-/* import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
- */
-import React, {Component} from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import "./style.css";
 
-class RangeBox extends Component {
+class RangeBox extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: 0
+    }
+  }
+
+  componentDidMount() {
+    this.setValue()
+  }
+
+  setValue() {
+    const { max } = this.props
+    const label = this.label
+    const inputSizes = ReactDOM.findDOMNode(this.input)
+    const value = parseInt(this.input.value)
+
+    this.setState({
+      inputValue: parseInt(this.input.value)
+    }, () => {
+      const percent = max ? value / max : value / 100
+      const thumbWidthSpace = 48
+      const newPoint = Math.abs(percent * (inputSizes.clientWidth - thumbWidthSpace))
+
+      label.style.left = newPoint  + "px"
+    })
+  }
+
   render() {
-    const { step, max, min, textThumb } = this.props
+    const { step, max, textThumb } = this.props
+    const { inputValue } = this.state
+
     return (
-      <div>
-        <input 
+      <div id="range-box-wrapper">
+        <input
           type="range"
           id="range-box"
           name="range-box"
-          min={min || 0}
+          min="0"
           max={max || 100}
-          step={step || 1} />
-        <label for="range-box">Label</label>
+          step={step || 1}
+          value={inputValue}
+          ref={input => this.input = input}
+          onChange={() => this.setValue()} />
+        <label
+          htmlFor="range-box"
+          ref={(label) => (this.label = label)}
+          className="range-box-label">
+          {inputValue}{textThumb}
+        </label>
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
+  watch:true,
   module: {
     rules: [
       {
